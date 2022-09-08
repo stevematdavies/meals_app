e4a78dfdcfde139ed722aa64ea31bccb271bead3
Stephen Davies <ext-stephen.davies@lianatech.com>
2022-09-08 14:12:39 +0300

    Adding the meal selections

--- lib/screens/category_meals_screen.dart
+++ lib/screens/category_meals_screen.dart
@@ -3,32 +3,26 @@ import 'package:meals_app/dummy_data.dart';
 import 'package:meals_app/widgets/category_meal_item.dart';
 
 import '../models/category.dart';
-import '../models/meal.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
-
   static const routeName = '/category-meals';
 
   const CategoryMealsScreen({Key? key}) : super(key: key);
 
-  Widget _mealForCategory(BuildContext ctx, int index, Meal meal){
-    return CategoryMealItem(meal);
-  }
-
   @override
   Widget build(BuildContext context) {
     final Category category =
-      ModalRoute.of(context)?.settings.arguments as Category;
-    final mealsForCategory = dummyMeals.where((meal) =>
-          meal.categories.contains(category.id)).toList();
+        ModalRoute.of(context)?.settings.arguments as Category;
+    final mealsForCategory = dummyMeals
+        .where((meal) => meal.categories.contains(category.id))
+        .toList();
     return Scaffold(
-      appBar: AppBar(
-        title:  Text(category.title),
-      ),
-      body: ListView.builder(
-          itemBuilder: (c,i) => _mealForCategory(c,i, mealsForCategory[i]),
-        itemCount: mealsForCategory.length,
-      )
-    );
+        appBar: AppBar(
+          title: Text(category.title),
+        ),
+        body: ListView.builder(
+          itemBuilder: (_, i) => CategoryMealItem(mealsForCategory[i]),
+          itemCount: mealsForCategory.length,
+        ));
   }
 }
--- lib/widgets/category_meal_item.dart
+++ lib/widgets/category_meal_item.dart
@@ -8,6 +8,63 @@ class CategoryMealItem extends StatelessWidget {
 
   void onMealSelect() {}
 
+
+  String get complexityLabel {
+    switch(meal.complexity){
+      case Complexity.simple:
+        return 'Simple';
+      case Complexity.challenging:
+        return 'Challenging';
+      case Complexity.hard:
+        return 'Hard';
+      default:
+        return 'Simple';
+    }
+  }
+
+  String get affordabilityLabel {
+    switch(meal.affordability){
+      case Affordability.affordable:
+        return 'Affordable';
+      case Affordability.pricey:
+        return 'Pricey';
+      case Affordability.luxurious:
+        return 'Luxurious';
+      default:
+        return 'Affordable';
+    }
+  }
+
+  List<Widget> get bottomBarItems {
+    return   [
+      Row(
+        children: [
+          const Icon(Icons.schedule),
+          const SizedBox(width: 6),
+          Text('${meal.duration} min'),
+        ],
+      ),
+
+      Row(
+        children: [
+          const Icon(Icons.work),
+          const SizedBox(width: 6),
+          Text(complexityLabel),
+        ],
+      ),
+
+      Row(
+        children: [
+          const Icon(Icons.euro_rounded),
+          const SizedBox(width: 6),
+          Text(affordabilityLabel),
+        ],
+      ),
+    ];
+  }
+
+
+
   @override
   Widget build(BuildContext context) {
     return InkWell(
@@ -32,8 +89,33 @@ class CategoryMealItem extends StatelessWidget {
                     width: double.infinity,
                     fit: BoxFit.cover,
                   ),
+                ),
+                Positioned(
+                  bottom: 20,
+                  right: 10,
+                  child: Container(
+                    width: 220,
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 5,
+                      horizontal: 20,
+                    ),
+                    color: Colors.black54,
+                    child: Text(
+                      meal.title,
+                      style: const TextStyle(fontSize: 26, color: Colors.white),
+                      softWrap: true,
+                      overflow: TextOverflow.fade,
+                    ),
+                  ),
                 )
               ],
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: bottomBarItems
+              ),
             )
           ],
         ),
