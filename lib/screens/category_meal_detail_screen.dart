c3c5020e5adfa10a3358d84ceb068c50fd3303ec
Stephen Davies <ext-stephen.davies@lianatech.com>
2022-09-08 15:06:54 +0300

    Adding steps

--- lib/screens/category_meal_detail_screen.dart
+++ lib/screens/category_meal_detail_screen.dart
@@ -8,6 +8,24 @@ class CategoryMealDetailScreen extends StatelessWidget {
 
   const CategoryMealDetailScreen({Key? key}) : super(key: key);
 
+  Widget _sectionTitle(String text, BuildContext context) => Container(
+        margin: const EdgeInsets.symmetric(vertical: 10),
+        child: Text(text, style: Theme.of(context).textTheme.headline6),
+      );
+
+  Widget _decoratedSectionContainer(Widget child) => Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border.all(color: Colors.grey),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        margin: const EdgeInsets.all(10),
+        padding: const EdgeInsets.all(10),
+        height: 150,
+        width: 300,
+        child: child,
+      );
+
   @override
   Widget build(BuildContext context) {
     final Meal meal = ModalRoute.of(context)?.settings.arguments as Meal;
@@ -25,39 +43,37 @@ class CategoryMealDetailScreen extends StatelessWidget {
                 fit: BoxFit.cover,
               ),
             ),
-            Container(
-              margin: const EdgeInsets.symmetric(vertical: 10),
-              child: Text('Ingredients',
-                  style: Theme.of(context).textTheme.headline6),
-            ),
-            Container(
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  border: Border.all(color: Colors.grey),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                margin: const EdgeInsets.all(10),
-                padding: const EdgeInsets.all(10),
-                height: 150,
-                width: 300,
-                child: ListView.builder(
-                  itemBuilder: (ctx, idx) {
-                    var ingredients = meal.ingredients[idx];
-                    return Card(
-                      color: Theme.of(context).colorScheme.secondary,
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(
-                          vertical: 5,
-                          horizontal: 10,
-                        ),
-                        child: Text(
-                          ingredients,
-                        ),
-                      ),
-                    );
-                  },
-                  itemCount: meal.ingredients.length,
-                ))
+            _sectionTitle('Ingredients', context),
+            _decoratedSectionContainer(ListView.builder(
+              itemBuilder: (ctx, idx) {
+                var ingredients = meal.ingredients[idx];
+                return Card(
+                  color: Theme.of(context).colorScheme.secondary,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 5,
+                      horizontal: 10,
+                    ),
+                    child: Text(
+                      ingredients,
+                    ),
+                  ),
+                );
+              },
+              itemCount: meal.ingredients.length,
+            )),
+            _sectionTitle('Instructions', context),
+            _decoratedSectionContainer(ListView.builder(
+                itemBuilder: (_, idx) {
+                  var step = meal.steps[idx];
+                  return ListTile(
+                    leading: CircleAvatar(
+                      child: Text('#${idx + 1}'),
+                    ),
+                    title: Text(step),
+                  );
+                },
+                itemCount: meal.steps.length))
           ],
         ));
   }
