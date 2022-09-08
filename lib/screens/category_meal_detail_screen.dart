43b5f896c4a4370f4fd03552d5df22f837902bef
Stephen Davies <ext-stephen.davies@lianatech.com>
2022-09-08 15:10:46 +0300

    Adding more

--- lib/screens/category_meal_detail_screen.dart
+++ lib/screens/category_meal_detail_screen.dart
@@ -33,48 +33,55 @@ class CategoryMealDetailScreen extends StatelessWidget {
         appBar: AppBar(
           title: Text(meal.title),
         ),
-        body: Column(
-          children: [
-            SizedBox(
-              height: 300,
-              width: double.infinity,
-              child: Image.network(
-                meal.imageUrl,
-                fit: BoxFit.cover,
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 300,
+                width: double.infinity,
+                child: Image.network(
+                  meal.imageUrl,
+                  fit: BoxFit.cover,
+                ),
               ),
-            ),
-            _sectionTitle('Ingredients', context),
-            _decoratedSectionContainer(ListView.builder(
-              itemBuilder: (ctx, idx) {
-                var ingredients = meal.ingredients[idx];
-                return Card(
-                  color: Theme.of(context).colorScheme.secondary,
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                      vertical: 5,
-                      horizontal: 10,
+              _sectionTitle('Ingredients', context),
+              _decoratedSectionContainer(ListView.builder(
+                itemBuilder: (ctx, idx) {
+                  var ingredients = meal.ingredients[idx];
+                  return Card(
+                    color: Theme.of(context).colorScheme.secondary,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 5,
+                        horizontal: 10,
+                      ),
+                      child: Text(
+                        ingredients,
+                      ),
                     ),
-                    child: Text(
-                      ingredients,
-                    ),
-                  ),
-                );
-              },
-              itemCount: meal.ingredients.length,
-            )),
-            _sectionTitle('Instructions', context),
-            _decoratedSectionContainer(ListView.builder(
-                itemBuilder: (_, idx) {
-                  var step = meal.steps[idx];
-                  return ListTile(
-                    leading: CircleAvatar(
-                      child: Text('#${idx + 1}'),
-                    ),
-                    title: Text(step),
                   );
                 },
-                itemCount: meal.steps.length))
-          ],
+                itemCount: meal.ingredients.length,
+              )),
+              _sectionTitle('Instructions', context),
+              _decoratedSectionContainer(ListView.builder(
+                  itemBuilder: (_, idx) {
+                    var step = meal.steps[idx];
+                    return Column(
+                      children: [
+                        ListTile(
+                          leading: CircleAvatar(
+                            child: Text('#${idx + 1}'),
+                          ),
+                          title: Text(step),
+                        ),
+                        const Divider()
+                      ],
+                    );
+                  },
+                  itemCount: meal.steps.length))
+            ],
+          ),
         ));
   }
 }
