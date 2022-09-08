8251caf04aff7913b9de76589aabe30683f6e1ea
Stephen Davies <ext-stephen.davies@lianatech.com>
2022-09-08 13:09:51 +0300

    pasomg in meal category data

--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/dummy_data.dart';
 
 import 'models/category.dart';
+import 'models/meal.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
 
@@ -8,19 +10,23 @@ class CategoryMealsScreen extends StatelessWidget {
 
   const CategoryMealsScreen({Key? key}) : super(key: key);
 
+  Widget _mealForCategory(BuildContext ctx, int index, List<Meal> meals){
+    return Text(meals[index].title);
+  }
+
   @override
   Widget build(BuildContext context) {
     final Category category =
       ModalRoute.of(context)?.settings.arguments as Category;
+    final mealsForCategory = dummyMeals.where((meal) =>
+          meal.categories.contains(category.id)).toList();
     return Scaffold(
       appBar: AppBar(
         title:  Text(category.title),
       ),
       body: ListView.builder(
-          itemBuilder: (ctx, idx) =>
-
-      ,
-        itemCount: ,
+          itemBuilder: (c,i) => _mealForCategory(c,i, mealsForCategory),
+        itemCount: mealsForCategory.length,
       )
     );
   }
