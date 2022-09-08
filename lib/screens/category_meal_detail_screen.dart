d23ff251b89bdd67d1e47eb2e3e3495c47ccb6d5
Stephen Davies <ext-stephen.davies@lianatech.com>
2022-09-08 14:52:39 +0300

    Adding ingredients

--- lib/screens/category_meal_detail_screen.dart
+++ lib/screens/category_meal_detail_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/dummy_data.dart';
 
 import '../models/meal.dart';
 
@@ -14,8 +15,50 @@ class CategoryMealDetailScreen extends StatelessWidget {
         appBar: AppBar(
           title: Text(meal.title),
         ),
-        body: Center(
-          child: Text(meal.id),
+        body: Column(
+          children: [
+            SizedBox(
+              height: 300,
+              width: double.infinity,
+              child: Image.network(
+                meal.imageUrl,
+                fit: BoxFit.cover,
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(vertical: 10),
+              child: Text('Ingredients',
+                  style: Theme.of(context).textTheme.headline6),
+            ),
+            Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border.all(color: Colors.grey),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                margin: const EdgeInsets.all(10),
+                padding: const EdgeInsets.all(10),
+                height: 150,
+                width: 300,
+                child: ListView.builder(
+                  itemBuilder: (ctx, idx) {
+                    var ingredients = meal.ingredients[idx];
+                    return Card(
+                      color: Theme.of(context).colorScheme.secondary,
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                          vertical: 5,
+                          horizontal: 10,
+                        ),
+                        child: Text(
+                          ingredients,
+                        ),
+                      ),
+                    );
+                  },
+                  itemCount: meal.ingredients.length,
+                ))
+          ],
         ));
   }
 }
